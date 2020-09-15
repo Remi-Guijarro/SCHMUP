1be7730df18fef87881321d30bb1a362df6d0c28
Remi-Guijarro <remi.guijarro@outlook.fr>
2020-09-15 16:56:40 +0200

    Created Boilerplate for Bullets :
    - Created Abstract class for Generic Bullets
    - Created Prefab for PlayerBullet
    - Created Prefab for EnemyBullet

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Bullet : MonoBehaviour
+{
+    [SerializeField]
+    private float damage;
+   
+    public float Damage
+    {
+        get
+        {
+            return this.damage;
+        }
+
+        set
+        {
+            this.damage = value;
+        }
+    }
+
+    private Vector2 speed;
+
+    public Vector2 Speed
+    {
+        get
+        {
+            return this.speed;
+        }
+
+        set
+        {
+            this.speed = value;
+        }
+    }
+
+    public Vector2 Position
+    {
+        get
+        {
+            return this.transform.position;
+        }
+
+        set
+        {
+            this.transform.position = value;    
+        }
+    }
+
+    public virtual void Init()
+    {
+
+    }
+
+    public virtual void UpdatePosition()
+    {
+
+    }
+
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
