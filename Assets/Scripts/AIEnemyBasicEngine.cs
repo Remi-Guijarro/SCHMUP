2bd1985835db94c0c4838bf6960eff8a875b6ec1
Remi-Guijarro <remi.guijarro@outlook.fr>
2020-09-15 16:30:46 +0200

    Implemented basic movements for Enemy

---
+++ Assets/Scripts/AIEnemyBasicEngine.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIEnemyBasicEngine : MonoBehaviour
+{
+    void Update()
+    {
+        float horizontalAxis = -1f;
+        float verticalAxis = 0f;
+        GetComponent<Engines>().Speed = new Vector2(horizontalAxis, verticalAxis);
+    }
+}
